9641ac24d845a0650c7d97f0c5c9cf868149f06b
Yure Rafael <yurerafael01@gmail.com>
2024-03-12 14:21:35 -0300

    add modals on page login

---
+++ web/js/login.js
@@ -0,0 +1,21 @@
+const input_email = document.getElementById('input-email')
+const input_password = document.getElementById('input-password')
+const btn_login = document.getElementById('btn-login')
+
+const modal_success = document.getElementById('modal-login-success')
+const modal_error = document.getElementById('modal-login-error')
+
+btn_login.addEventListener('click', () => {
+  if(!input_email.value || !input_password.value) {
+    modal_error.style.display = 'flex'
+    setTimeout(() => {
+      modal_error.style.display = 'none'
+    }, 3000)
+  } 
+  else if(input_email.value && input_password.value){
+    modal_success.style.display = 'flex'
+    setTimeout(() => {
+      modal_success.style.display = 'none'
+    }, 3000)
+  }
+})
