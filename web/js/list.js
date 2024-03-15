0c51bc1bbdd19868a5e635e9ebb0e330d9edc309
Yure Rafael <yurerafael01@gmail.com>
2024-03-15 15:17:56 -0300

    add modal to invite people and remove task useless

--- web/js/list.js
+++ web/js/list.js
@@ -17,6 +17,12 @@ const btn_add_to_do = document.getElementById("btn-add-to-do")
 const btn_add_new_task = document.getElementById("add-new-task")
 const btn_close_modal_task = document.getElementById("btn-close-modal-task")
 
+const modal_invite_people = document.getElementById("modal-invite-people")
+const email_invite = document.getElementById("email-invite")
+const name_invite = document.getElementById("name-invite")
+const btn_invite_people_inside = document.getElementById("btn-invite-people-inside")
+const btn_invite_people_outside = document.getElementById("btn-invite-people-outside")
+const btn_close_modal_invite = document.getElementById("btn-close-modal-invite")
 
 const lists = [
   {
@@ -52,8 +58,10 @@ btn_save_list.addEventListener('click', () => {
   modal_add_list.style.display = 'none'
 })
 
-btn_add_to_do.addEventListener('click', () =>{
-  modal_add_task.style.display = 'flex'
+btn_add_to_do.addEventListener('click', () => {
+  if(modal_add_task.style.display === 'flex'){
+    modal_add_task.style.display = 'none'  
+  } else modal_add_task.style.display = 'flex'
 })
 
 btn_close_modal_task.addEventListener('click', () => {
@@ -68,6 +76,21 @@ btn_add_new_task.addEventListener('click', () => {
     description: description_task
   } */
   modal_add_task.style.display = 'none'
+})
+
+btn_invite_people_inside.addEventListener('click', () => {
+  //if(!email_invite.value || !name_invite.value) return
+  modal_invite_people.style.display = 'none'
+})
+
+btn_invite_people_outside.addEventListener('click', () => {
+  if(modal_invite_people.style.display === 'flex'){
+    modal_invite_people.style.display = 'none'
+  } else modal_invite_people.style.display = 'flex'
+})
+
+btn_close_modal_invite.addEventListener('click', () => {
+  modal_invite_people.style.display = 'none'
 })
 
 const defineValues = () => {
