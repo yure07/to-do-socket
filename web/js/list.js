4a5bf04f871ba5b7899e7853033a749fd0a3929f
Yure Rafael <yurerafael01@gmail.com>
2024-03-25 18:55:56 -0300

    order tasks of database on interface and starting to create + save lists

--- web/js/list.js
+++ web/js/list.js
@@ -3,15 +3,6 @@ const icon_previous_home = document.getElementById("icon-previous")
 
 const modal_add_list = document.getElementById("modal-add-list")
 
-const btn_open_modal = document.getElementById("btn-open-modal")
-const btn_close_modal = document.getElementById("btn-close-modal")
-const btn_save_list = document.getElementById("btn-add-new-list")
-const start_new_list_text = document.getElementById("start-new-list-text")
-
-const title_list = document.getElementById("title-list")
-const member_list = document.getElementById("member-list")
-const description_list = document.getElementById("description-list")
-
 const modal_add_task = document.getElementById("modal-add-task")
 const btn_add_to_do = document.getElementById("btn-add-to-do")
 
@@ -24,42 +15,8 @@ const btn_invite_people_inside = document.getElementById("btn-invite-people-insi
 const btn_invite_people_outside = document.getElementById("btn-invite-people-outside")
 const btn_close_modal_invite = document.getElementById("btn-close-modal-invite")
 
-const lists = [
-  {
-    title: 'Estudos',
-    member: 'Yure',
-    description: 'Próximos tópicos a estudar na programação'
-  }
-]
-
-const tasks = [
-  {
-
-  }
-]
-
 icon_previous_home.addEventListener('click', () => {
   window.location.href = '/web/pages/login.html'
-})
-
-btn_open_modal.addEventListener('click', () => {
-  modal_add_list.style.display = 'flex'
-  start_new_list_text.style.display = 'none'
-})
-
-btn_close_modal.addEventListener('click', () => {
-  modal_add_list.style.display = 'none'
-  start_new_list_text.style.display = 'flex'
-})
-
-start_new_list_text.addEventListener('click', () => {
-  modal_add_list.style.display = 'flex'
-  start_new_list_text.style.display = 'none'
-})
-
-btn_save_list.addEventListener('click', () => {
-  //if(!title_list.value || !member_list.value || !description_list.value) return
-  modal_add_list.style.display = 'none'
 })
 
 btn_add_to_do.addEventListener('click', () => {
@@ -71,17 +28,6 @@ btn_add_to_do.addEventListener('click', () => {
 btn_close_modal_task.addEventListener('click', () => {
   modal_add_task.style.display = 'none'
 })
-
-/*
-btn_add_new_task.addEventListener('click', () => {
-  if(!title_task.value || !member_task.value || !description_task.value) return
-  const new_task = {
-    title: title_task.value,
-    member: member_task.value,
-    description: description_task
-  } 
-  modal_add_task.style.display = 'none'
-})*/
 
 btn_invite_people_inside.addEventListener('click', () => {
   //if(!email_invite.value || !name_invite.value) return
@@ -97,13 +43,3 @@ btn_invite_people_outside.addEventListener('click', () => {
 btn_close_modal_invite.addEventListener('click', () => {
   modal_invite_people.style.display = 'none'
 })
-
-const defineValues = () => {
-  lists.map((list) => {
-    title_list.innerHTML = list.title
-  })
-  const urlPhoto = localStorage.getItem('@urlImg_user')
-  img_profile_user.src = urlPhoto
-}
-
-window.onload = defineValues()
